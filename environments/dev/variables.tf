a23365438dee83244e5e51750a85a0f65ca4b78b
saurabh <chanchlesh2597@gmail.com>
2026-04-11 19:12:57 +0530

    added fixes to ci

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,16 +1,3 @@
-variable "db_password" {
-  type        = string
-  sensitive   = true
-}
-
-variable "vpc_id" {
-  type = string
-}
-
-variable "private_subnet_ids" {
-  type = list(string)
-}
-
 
 variable "aws_region" {
   default = "us-east-1"
